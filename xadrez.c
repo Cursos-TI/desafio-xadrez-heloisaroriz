ecd8c52c6e89e4187c1b4ccdb6d42e8f56c83b4a
heloisaroriz <heloisaroriz67@gmail.com>
2025-04-10 03:29:06 +0000

    novato

--- xadrez.c
+++ xadrez.c
@@ -10,21 +10,21 @@ int main() {
     printf("Movimentos das peças de xadrez \n");
 
     printf("\n----------Movimento da Torre---------- \n\n");
-    for( int torre = 0; torre < 5; torre++) //Vai execultar 5 vezes o movimeto da torre
+    for( int torre = 0; torre < 5; torre++) //Vai execultar 5 vezes o movimento da torre
     {
        printf("A torre se moveu para a direita \n");
     }
 
     printf("\n----------Movimento do Bispo----------\n\n");
 
-    while (bispo < 5) { //Vai execultar 5 vezes o movimeto do bispo
+    while (bispo < 5) { //Vai execultar 5 vezes o movimento do bispo
         printf("O bispo se moveu para cima, direita \n");
         bispo++;
     }
     
     printf("\n----------Movimento da Rainha----------\n\n");
 
-    do{ //Vai execultar 8 vezes o movimeto da rainha
+    do{ //Vai execultar 8 vezes o movimento da rainha
        printf("A rainha se moveu para a esquerda \n");
        rainha++;
     } while (rainha < 8);
