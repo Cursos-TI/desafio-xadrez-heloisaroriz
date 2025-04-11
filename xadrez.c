1fc9a0f4be5c4845fbf85c5692bb11f193a4eebe
heloisaroriz <heloisaroriz67@gmail.com>
2025-04-11 01:19:56 +0000

    nivel 2

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@
 // Nível Novato - Movimentação das Peças
    
 int main() {
-    int bispo = 0, rainha = 0;
+    int bispo = 0, rainha = 0, cavalo = 1;
 
 
     printf("Movimentos das peças de xadrez \n");
@@ -12,29 +12,42 @@ int main() {
     printf("\n----------Movimento da Torre---------- \n\n");
     for( int torre = 0; torre < 5; torre++) //Vai execultar 5 vezes o movimento da torre
     {
-       printf("A torre se moveu para a direita \n");
+       printf("Direita \n");
     }
+    printf("Torre andou 5 casas para direita\n");
 
     printf("\n----------Movimento do Bispo----------\n\n");
 
     while (bispo < 5) { //Vai execultar 5 vezes o movimento do bispo
-        printf("O bispo se moveu para cima, direita \n");
+        printf("Cima, Direita \n");
         bispo++;
     }
+    printf("Bispo andou 5 casas em diagonal\n");
     
     printf("\n----------Movimento da Rainha----------\n\n");
 
     do{ //Vai execultar 8 vezes o movimento da rainha
-       printf("A rainha se moveu para a esquerda \n");
+       printf("Esquerda \n");
        rainha++;
     } while (rainha < 8);
-    
+    printf("Rainha andou 8 casas para esquerda\n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
+
+// Nível Aventureiro - Movimentação do Cavalo
+    printf("\n----------Movimento do Cavalo----------\n\n");
+
+    while (cavalo--)
+    {
+        for (int c = 0; c < 2; c++) {
+          printf("Baixo, ");    
+
+        }
+        printf("Esquerda\n");
+    }
+    printf("Cavalo moveu em L\n");
+        
+// Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
