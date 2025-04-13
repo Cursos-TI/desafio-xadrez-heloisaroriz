ae5762cba0ff5d6096faba125ee60a73f2ce19a8
heloisaroriz <heloisaroriz67@gmail.com>
2025-04-13 16:48:58 +0000

    nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -2,13 +2,14 @@
 
 // Desafio de Xadrez - MateCheck
 // Nível Novato - Movimentação das Peças
-void recursivotorre(int torre) {
-    if (torre < 5) {
-        printf("Direita\n");
-        recursivotorre(torre - 1);
+void recursivotorre(int torre){
+    if (torre == 0){
+        printf("\nTorre moveu 5 casas para direita\n\n");
+        return;
     } 
+    printf("Direita\t");
+    recursivotorre(torre - 1);
 }
-
 void recursivobispo(int linhas, int colunas,int casas){
     if (casas == 0){
         printf("\nBispo moveu 5 casas em diagonal\n\n");
@@ -21,7 +22,7 @@ void recursivobispo(int linhas, int colunas,int casas){
             }
         }
     }
-    mov_bispo(0,0,casas - 1);
+    recursivobispo(0,0,casas - 1);
 }
 void recessivorainha(int casas){
     if (casas == 0){
@@ -31,12 +32,14 @@ void recessivorainha(int casas){
     printf("Esquerda\t");
     recessivorainha(casas - 1);
 }
+
+
 void recessivocavalo(int movimentos) {
     for (int i = 0; i < movimentos; i++) {
         for (int j = 0; j < movimentos; j++) {
             if (i == j) continue;
             if (i == 2 && j == 1) {
-                printf("Cima\tCima\tDireita");
+                printf("Cima, Cima, Direita\t");
                 break;
             }
         }
